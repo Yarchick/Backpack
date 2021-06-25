4edab93f2170094e99684df18ad9269ce970fd69
Yarchick <yyarchick@gmail.com>
2021-06-25 16:08:14 +0300

    Create build

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     filename: "js/game0.1.js",
-    path: path.resolve(__dirname, "../build")
+    path: path.resolve(__dirname, "../docs")
   },
   module: {
     rules: [
